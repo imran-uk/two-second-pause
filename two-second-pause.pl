c3a0261e08b96004f5395e8991a372f91095a17a
Imran Chaudhry <ichaudhry@gmail.com>
2011-02-13 14:00:51 +0000

    Force 80 char width of docs.

--- two-second-pause.pl
+++ two-second-pause.pl
@@ -14,11 +14,19 @@ Run: ./two-second-pause.pl
 
 =head1 DESCRIPTION
 
-Attempts to detect a track change in Rhythmbox and insert a 2-second pause at those points. It uses rhythmbox-client in an endless loop, this causes a high load average due to continual chatter to the dbus daemon. It is mainly to be used with minidisc recorders connected via an analogue or digital connection while a playlist is played. Occasionally two trackmarks will be inserted right next to each other. However the goal of marking each song for ease of tracking during playback is achieved.
+Attempts to detect a track change in Rhythmbox and insert a 2-second pause at
+those points. It uses rhythmbox-client in an endless loop, this causes a high
+load average due to continual chatter to the dbus daemon. It is mainly to be
+used with minidisc recorders connected via an analogue or digital connection
+while a playlist is played. Occasionally two trackmarks will be inserted right
+next to each other. However the goal of marking each song for ease of tracking
+during playback is achieved.
 
-In it's terminal window, two-second-pause.pl will output the currently playing track at each title change point.
+In it's terminal window, two-second-pause.pl will output the currently playing
+track at each title change point.
 
-Change the $PAUSE parameter (unit of seconds) if you wish to produce a longer pause.
+Change the $PAUSE parameter (unit of seconds) if you wish to produce a longer
+pause.
 
 =cut
 
